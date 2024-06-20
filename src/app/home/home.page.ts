8eca90b0f41ad44a0c670acf00676fa58d79dcf9
Ken Sodemann <ken@ionic.io>
2024-06-20 14:50:35 -0500

    implement the error stuffs

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCheckbox, IonNote } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonCheckbox, IonNote, IonHeader, IonToolbar, IonTitle, IonContent, FormsModule],
 })
 export class HomePage {
+  @ViewChild('plainCB') plainCB!: ElementRef;
+  @ViewChild('ionicCB') ionicCB!: any;
+
+  isError: Boolean = false;
+
   constructor() {}
+
+  errorChanged() {
+    this.plainCB.nativeElement.setAttribute('aria-invalid', this.isError);
+    const input = this.ionicCB.elementRef.nativeElement.shadowRoot?.querySelector("input[type='checkbox']");
+    input.setAttribute('aria-invalid', this.isError);
+  }
 }
